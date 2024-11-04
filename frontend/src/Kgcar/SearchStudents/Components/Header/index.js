1ef18144d6c5d9bfbd187433a3f2aa5d1ff16a88
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-11-04 10:42:54 +0530

    change in ui

--- frontend/src/Kgcar/SearchStudents/Components/Header/index.js
+++ frontend/src/Kgcar/SearchStudents/Components/Header/index.js
@@ -10,6 +10,7 @@ import SoftTypography from "components/SoftTypography";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import breakpoints from "assets/theme/base/breakpoints";
 import axios from "axios"; 
+import './index.css'
 import Snackbar from "@mui/material/Snackbar"; 
 import Alert from "@mui/material/Alert"; 
 import SoftButton from "components/SoftButton"; // Ensure you have a button component
@@ -58,48 +59,66 @@ function DocSearchHeader({ setSearchResults }) {
   };
 
   return (
-    <SoftBox position="relative">
-      <DashboardNavbar absolute light />
-      <SoftBox display="flex" alignItems="center" position="relative" minHeight="18.75rem">
-        <Card sx={{ mt: -8, mx: 3, py: 2, px: 2 }}>
-          <Grid container spacing={3} alignItems="center">
-            <Grid item xs={6}>
-              <SoftTypography variant="h4" fontWeight="bold">SEARCH STUDENTS</SoftTypography>
-            </Grid>
-            <Grid item xs={"100%"} style={{ display: "flex", justifyContent: "flex-end" }}>
-              <SoftBox pr={6}>
-                <TextField
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  placeholder="Enter admission no..."
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <SearchIcon />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </SoftBox>
-              <SoftButton onClick={handleSearch} variant="contained" color="light">
-                Search
-              </SoftButton>
-            </Grid>
-          </Grid>
-        </Card>
-      </SoftBox>
+    // <SoftBox position="relative">
+    //   <DashboardNavbar absolute light />
+    //   <SoftBox display="flex" justifyContent="space-between" alignItems="center" position="relative" minHeight="18.75rem" width="100%" >
+    //     <Card sx={{ mt: -8, mx: 3, py: 2, px: 2 }}>
+    //       <Grid container spacing={3} alignItems="center">
+    //         <Grid item xs={6}>
+    //           <SoftTypography variant="h4" fontWeight="bold">SEARCH STUDENTS</SoftTypography>
+    //         </Grid>
+    //         <Grid item xs={"100%"} style={{ display: "flex", justifyContent: "flex-end" }}>
+    //           <SoftBox pr={6}>
+    //             <TextField
+    //               value={searchTerm}
+    //               onChange={(e) => setSearchTerm(e.target.value)}
+    //               placeholder="Enter admission no..."
+    //               InputProps={{
+    //                 startAdornment: (
+    //                   <InputAdornment position="start">
+    //                     <SearchIcon />
+    //                   </InputAdornment>
+    //                 ),
+    //               }}
+    //             />
+    //           </SoftBox>
+    //           <SoftButton onClick={handleSearch} variant="contained" color="info">
+    //             Search
+    //           </SoftButton>
+    //         </Grid>
+    //       </Grid>
+    //     </Card>
+    //   </SoftBox>
 
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={6000}
-        onClose={handleCloseSnackbar}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      >
-        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
-          {errorMessage}
-        </Alert>
-      </Snackbar>
-    </SoftBox>
+    //   <Snackbar
+    //     open={openSnackbar}
+    //     autoHideDuration={6000}
+    //     onClose={handleCloseSnackbar}
+    //     anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    //   >
+    //     <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+    //       {errorMessage}
+    //     </Alert>
+    //   </Snackbar>
+    // </SoftBox>
+    <>
+      <div className="searchbox-container">
+        <div className="searchbox">
+          <div className="searchbox-text"><h1>Search Student</h1></div>
+          <div className="searchbox__input">
+            <input
+              type="text"
+              placeholder="Enter admission no..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <div className="searchbox__button">
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- frontend/src/layouts/tables/data/authorsTableData.js
+++ frontend/src/layouts/tables/data/authorsTableData.js
@@ -1,196 +1,196 @@
-/* eslint-disable react/prop-types */
-// Soft UI Dashboard React components
-import SoftBox from "components/SoftBox";
-import SoftTypography from "components/SoftTypography";
-import SoftAvatar from "components/SoftAvatar";
-import SoftBadge from "components/SoftBadge";
+  /* eslint-disable react/prop-types */
+  // Soft UI Dashboard React components
+  import SoftBox from "components/SoftBox";
+  import SoftTypography from "components/SoftTypography";
+  import SoftAvatar from "components/SoftAvatar";
+  import SoftBadge from "components/SoftBadge";
 
-// Images
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+  // Images
+  import team2 from "assets/images/team-2.jpg";
+  import team3 from "assets/images/team-3.jpg";
+  import team4 from "assets/images/team-4.jpg";
 
-function Author({ image, name, email }) {
-  return (
-    <SoftBox display="flex" alignItems="center" px={1} py={0.5}>
-      <SoftBox mr={2}>
-        <SoftAvatar src={image} alt={name} size="sm" variant="rounded" />
+  function Author({ image, name, email }) {
+    return (
+      <SoftBox display="flex" alignItems="center" px={1} py={0.5}>
+        <SoftBox mr={2}>
+          <SoftAvatar src={image} alt={name} size="sm" variant="rounded" />
+        </SoftBox>
+        <SoftBox display="flex" flexDirection="column">
+          <SoftTypography variant="button" fontWeight="medium">
+            {name}
+          </SoftTypography>
+          <SoftTypography variant="caption" color="secondary">
+            {email}
+          </SoftTypography>
+        </SoftBox>
       </SoftBox>
+    );
+  }
+
+  function Function({ job, org }) {
+    return (
       <SoftBox display="flex" flexDirection="column">
-        <SoftTypography variant="button" fontWeight="medium">
-          {name}
+        <SoftTypography variant="caption" fontWeight="medium" color="text">
+          {job}
         </SoftTypography>
         <SoftTypography variant="caption" color="secondary">
-          {email}
+          {org}
         </SoftTypography>
       </SoftBox>
-    </SoftBox>
-  );
-}
+    );
+  }
 
-function Function({ job, org }) {
-  return (
-    <SoftBox display="flex" flexDirection="column">
-      <SoftTypography variant="caption" fontWeight="medium" color="text">
-        {job}
-      </SoftTypography>
-      <SoftTypography variant="caption" color="secondary">
-        {org}
-      </SoftTypography>
-    </SoftBox>
-  );
-}
+  const authorsTableData = {
+    columns: [
+      { name: "Student", align: "left" },
+      { name: "Admission_No", align: "left" },
+      { name: "status", align: "center" },
+      { name: "employed", align: "center" },
+      { name: "action", align: "center" },
+    ],
 
-const authorsTableData = {
-  columns: [
-    { name: "author", align: "left" },
-    { name: "function", align: "left" },
-    { name: "status", align: "center" },
-    { name: "employed", align: "center" },
-    { name: "action", align: "center" },
-  ],
+    rows: [
+      {
+        Student: <Author name="Yogesh"/>,
+        Admission_No: <Function job="12345" />,
+        status: (
+          <SoftBadge variant="gradient">CSE</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      {
+        Student: <Author name="Raja Rajan"/>,
+        Admission_No: <Function job="12245" />,
+        status: (
+          <SoftBadge variant="gradient">CSE</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      {
+        Student: <Author name="Rajiv"/>,
+        Admission_No: <Function job="122345" />,
+        status: (
+          <SoftBadge variant="gradient">CSE</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      {
+        Student: <Author name="Nishanth"/>,
+        Admission_No: <Function job="123452" />,
+        status: (
+          <SoftBadge variant="gradient">CSE</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      {
+        Student: <Author name="Mitun"/>,
+        Admission_No: <Function job="12245" />,
+        status: (
+          <SoftBadge variant="gradient">CSBS</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      {
+        Student: <Author name="Sivakanesh"/>,
+        Admission_No: <Function job="1234523" />,
+        status: (
+          <SoftBadge variant="gradient">IT</SoftBadge>
+        ),
+        employed: (
+          <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+            3
+          </SoftTypography>
+        ),
+        action: (
+          <SoftTypography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium"
+          >
+            Finish
+          </SoftTypography>
+        ),
+      },
+      
+    ],
+  };
 
-  rows: [
-    {
-      author: <Author name="Yogesh"/>,
-      function: <Function job="12345" />,
-      status: (
-        <SoftBadge variant="gradient">CSE</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    {
-      author: <Author name="Raja Rajan"/>,
-      function: <Function job="12245" />,
-      status: (
-        <SoftBadge variant="gradient">CSE</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    {
-      author: <Author name="Rajiv"/>,
-      function: <Function job="122345" />,
-      status: (
-        <SoftBadge variant="gradient">CSE</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    {
-      author: <Author name="Nishanth"/>,
-      function: <Function job="123452" />,
-      status: (
-        <SoftBadge variant="gradient">CSE</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    {
-      author: <Author name="Mitun"/>,
-      function: <Function job="12245" />,
-      status: (
-        <SoftBadge variant="gradient">CSBS</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    {
-      author: <Author name="Sivakanesh"/>,
-      function: <Function job="1234523" />,
-      status: (
-        <SoftBadge variant="gradient">IT</SoftBadge>
-      ),
-      employed: (
-        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-          3
-        </SoftTypography>
-      ),
-      action: (
-        <SoftTypography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium"
-        >
-          Finish
-        </SoftTypography>
-      ),
-    },
-    
-  ],
-};
-
-export default authorsTableData;
+  export default authorsTableData;
